47c830fb406b133be428ead23732f4ce68ef90b0
emkor93 <emkor93@gmail.com>
2023-12-01 09:55:07 +0100

    add first tests and impl for day 01 part 2

--- aoc23/day_01_01.py
+++ aoc23/day_01_01.py
@@ -1,6 +1,19 @@
+import collections
 import typing
 
 from aoc23.util import day_01_input_lines
+
+WORD_DIGITS = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
 
 def day_01_pt1_parse_calibration_value(text: str) -> int:
@@ -8,13 +21,22 @@ def day_01_pt1_parse_calibration_value(text: str) -> int:
     return int(f"{digits[0]}{digits[-1]}")
 
 
-def day_01_pt1_sum_calibration_values(texts: typing.Iterable[str]) -> int:
+def day_01_pt1_calibration_sum(texts: typing.Iterable[str]) -> int:
     return sum((day_01_pt1_parse_calibration_value(t) for t in texts))
 
 
-def day_01_pt1_answer() -> int:
-    return day_01_pt1_sum_calibration_values(texts=day_01_input_lines())
+def day_01_pt2_parse_valibartion_value(text: str) -> int:
+    digit_position_to_val = {p: int(ch) for p, ch in enumerate(text) if ch.isdigit()}
+    word_digit_pos_to_val = {text.find(w): v for w, v in WORD_DIGITS.items() if text.find(w) != -1}
+    merged = collections.OrderedDict(sorted({**digit_position_to_val, **word_digit_pos_to_val}.items()))
+    first_digit, last_digit = merged[min(merged.keys())], merged[max(merged.keys())]
+    return int(f"{first_digit}{last_digit}")
+
+
+def day_01_pt2_calibration_sum(texts: typing.Iterable[str]) -> int:
+    return sum((day_01_pt2_parse_valibartion_value(t) for t in texts))
 
 
 if __name__ == '__main__':
-    print(f"Day 01, part 1 answer: {day_01_pt1_answer()}")
+    print(f"Day 01, part 1 answer: {day_01_pt1_calibration_sum(texts=day_01_input_lines())}")
+    print(f"Day 01, part 2 answer: {day_01_pt2_calibration_sum(texts=day_01_input_lines())}")
--- aoc23/test/test_day_01_01.py
+++ aoc23/test/test_day_01_01.py
@@ -1,15 +1,29 @@
 import unittest
 
-from aoc23.day_01_01 import day_01_pt1_parse_calibration_value, day_01_pt1_sum_calibration_values
+from aoc23.day_01_01 import day_01_pt1_parse_calibration_value, day_01_pt1_calibration_sum, \
+    day_01_pt2_parse_valibartion_value, day_01_pt2_calibration_sum
 
 
 class TestAocDay01Tests(unittest.TestCase):
 
-    def test_day_01_should_get_first_last_digit(self):
+
+    def test_day_01_pt1_should_get_first_last_digit(self):
         assert day_01_pt1_parse_calibration_value("1abc2") == 12
         assert day_01_pt1_parse_calibration_value("pqr3stu8vwx") == 38
         assert day_01_pt1_parse_calibration_value("a1b2c3d4e5f") == 15
         assert day_01_pt1_parse_calibration_value("treb7uchet") == 77
 
-    def test_day_01_should_sum_values(self):
-        assert day_01_pt1_sum_calibration_values(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]) == 142
+    def test_day_01_pt1_should_sum_values(self):
+        assert day_01_pt1_calibration_sum(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]) == 142
+
+    def test_day_01_pt2_should_get_first_last_digit(self):
+        assert day_01_pt2_parse_valibartion_value("two1nine") == 29
+        assert day_01_pt2_parse_valibartion_value("eightwothree") == 83
+        assert day_01_pt2_parse_valibartion_value("abcone2threexyz") == 13
+        assert day_01_pt2_parse_valibartion_value("xtwone3four") == 24
+        assert day_01_pt2_parse_valibartion_value("4nineeightseven2") == 42
+        assert day_01_pt2_parse_valibartion_value("zoneight234") == 14
+        assert day_01_pt2_parse_valibartion_value("7pqrstsixteen") == 76
+
+    def test_day_01_pt2_should_sum_values(self):
+        assert day_01_pt2_calibration_sum(["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]) == 281
