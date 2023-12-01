77b6b90b404f9a17881d78ca59d69127a5d46ee9
emkor93 <emkor93@gmail.com>
2023-12-01 09:22:56 +0100

    refactor naming for parts 1 and 2

--- aoc23/day_01_01.py
+++ aoc23/day_01_01.py
@@ -3,18 +3,18 @@ import typing
 from aoc23.util import day_01_input_lines
 
 
-def day_01_first_last_digit(text: str) -> int:
+def day_01_pt1_parse_calibration_value(text: str) -> int:
     digits = list(filter(lambda c: c.isdigit(), text))
     return int(f"{digits[0]}{digits[-1]}")
 
 
-def day_01_sum_calibration_values(texts: typing.Iterable[str]) -> int:
-    return sum((day_01_first_last_digit(t) for t in texts))
+def day_01_pt1_sum_calibration_values(texts: typing.Iterable[str]) -> int:
+    return sum((day_01_pt1_parse_calibration_value(t) for t in texts))
 
 
-def day_01_answer() -> int:
-    return day_01_sum_calibration_values(texts=day_01_input_lines())
+def day_01_pt1_answer() -> int:
+    return day_01_pt1_sum_calibration_values(texts=day_01_input_lines())
 
 
 if __name__ == '__main__':
-    print(f"Day 01: {day_01_answer()}")
+    print(f"Day 01, part 1 answer: {day_01_pt1_answer()}")
--- aoc23/test/test_day_01_01.py
+++ aoc23/test/test_day_01_01.py
@@ -1,15 +1,15 @@
 import unittest
 
-from aoc23.day_01_01 import day_01_first_last_digit, day_01_sum_calibration_values
+from aoc23.day_01_01 import day_01_pt1_parse_calibration_value, day_01_pt1_sum_calibration_values
 
 
 class TestAocDay01Tests(unittest.TestCase):
 
     def test_day_01_should_get_first_last_digit(self):
-        assert day_01_first_last_digit("1abc2") == 12
-        assert day_01_first_last_digit("pqr3stu8vwx") == 38
-        assert day_01_first_last_digit("a1b2c3d4e5f") == 15
-        assert day_01_first_last_digit("treb7uchet") == 77
+        assert day_01_pt1_parse_calibration_value("1abc2") == 12
+        assert day_01_pt1_parse_calibration_value("pqr3stu8vwx") == 38
+        assert day_01_pt1_parse_calibration_value("a1b2c3d4e5f") == 15
+        assert day_01_pt1_parse_calibration_value("treb7uchet") == 77
 
     def test_day_01_should_sum_values(self):
-        assert day_01_sum_calibration_values(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]) == 142
+        assert day_01_pt1_sum_calibration_values(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]) == 142
