499d958fe608cf6466c186761520808abf426f68
emkor93 <emkor93@gmail.com>
2023-12-01 09:12:42 +0100

    add function for summing up results

--- aoc23/day_01_01.py
+++ aoc23/day_01_01.py
@@ -1,3 +1,7 @@
-def day_01_first_last_digit(text: str) -> str:
+def day_01_first_last_digit(text: str) -> int:
     digits = list(filter(lambda c: c.isdigit(), text))
-    return f"{digits[0]}{digits[-1]}"
+    return int(f"{digits[0]}{digits[-1]}")
+
+
+def day_01_sum_calibration_values(texts: list[str]) -> int:
+    return sum((day_01_first_last_digit(t) for t in texts))
--- aoc23/test/test_day_01_01.py
+++ aoc23/test/test_day_01_01.py
@@ -1,12 +1,15 @@
 import unittest
 
-from aoc23.day_01_01 import day_01_first_last_digit
+from aoc23.day_01_01 import day_01_first_last_digit, day_01_sum_calibration_values
 
 
 class TestAocDay01Tests(unittest.TestCase):
 
     def test_day_01_should_get_first_last_digit(self):
-        assert day_01_first_last_digit("1abc2") == "12"
-        assert day_01_first_last_digit("pqr3stu8vwx") == "38"
-        assert day_01_first_last_digit("a1b2c3d4e5f") == "15"
-        assert day_01_first_last_digit("treb7uchet") == "77"
+        assert day_01_first_last_digit("1abc2") == 12
+        assert day_01_first_last_digit("pqr3stu8vwx") == 38
+        assert day_01_first_last_digit("a1b2c3d4e5f") == 15
+        assert day_01_first_last_digit("treb7uchet") == 77
+
+    def test_day_01_should_sum_values(self):
+        assert day_01_sum_calibration_values(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]) == 142
