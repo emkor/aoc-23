41f38e612e8b378f711ab5fc36046b312d630eb7
emkor93 <emkor93@gmail.com>
2023-12-01 09:20:51 +0100

    finish part 1 day 1

--- aoc23/day_01_01.py
+++ aoc23/day_01_01.py
@@ -1,7 +1,20 @@
+import typing
+
+from aoc23.util import day_01_input_lines
+
+
 def day_01_first_last_digit(text: str) -> int:
     digits = list(filter(lambda c: c.isdigit(), text))
     return int(f"{digits[0]}{digits[-1]}")
 
 
-def day_01_sum_calibration_values(texts: list[str]) -> int:
+def day_01_sum_calibration_values(texts: typing.Iterable[str]) -> int:
     return sum((day_01_first_last_digit(t) for t in texts))
+
+
+def day_01_answer() -> int:
+    return day_01_sum_calibration_values(texts=day_01_input_lines())
+
+
+if __name__ == '__main__':
+    print(f"Day 01: {day_01_answer()}")
---
+++ aoc23/util.py
@@ -0,0 +1,16 @@
+import fileinput
+import typing
+from os import path
+
+PROJ_NAME = "aoc-23"
+
+
+def proj_path(rel_to_proj: str) -> str:
+    dir_path = path.realpath(__file__).rpartition(PROJ_NAME)
+    return path.join(dir_path[0], dir_path[1], rel_to_proj)
+
+
+def day_01_input_lines() -> typing.Iterable[str]:
+    for line in fileinput.input(files=proj_path("input/day_01_calibration.txt"), encoding="utf-8"):
+        if line.strip():  # eliminate empty newline at the end
+            yield line.strip()
