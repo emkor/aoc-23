a533909662e82d98670caa73a9084a71650018c6
emkor93 <emkor93@gmail.com>
2023-12-04 10:27:48 +0100

    implemented unoptimal solution

--- aoc23/day_04.py
+++ aoc23/day_04.py
@@ -4,9 +4,9 @@ import typing
 from aoc23.util import input_lines
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class Card:
-    ix: int
+    ix: int = dataclasses.field(compare=True, hash=True)
     winning: set[int]
     got: set[int]
 
@@ -30,5 +30,31 @@ def day_04_pt1_answer(lines: typing.Iterable[str]) -> int:
     return sum((Card.parse(l).points() for l in lines))
 
 
+def day_04_pt2_answer(lines: typing.Iterable[str]) -> int:
+
+    deck: dict[int, Card] = {c.ix: c for c in (Card.parse(l) for l in lines)}
+    not_winning_ids = set()
+    hand: list[int] = sorted(list(deck.keys()))
+    i = 0
+
+    while set(hand[i:]) != not_winning_ids and i < len(hand):
+        print(f"#{i} hand len={len(hand)} not_winning_ids={not_winning_ids}")
+        curr_card = deck[hand[i]]
+        new_hand = _resolve_new_hand(curr_card, not_winning_ids)
+        hand.extend(new_hand)
+        i += 1
+    return len(hand)
+
+
+def _resolve_new_hand(curr_card: Card, not_winning_ids: set[int]):
+    new_hand = []
+    for next_card_ix in range(curr_card.ix + 1, curr_card.ix + 1 + curr_card.wins()):
+        new_hand.append(next_card_ix)
+    if not len(new_hand):
+        not_winning_ids.add(curr_card.ix)
+    return new_hand
+
+
 if __name__ == '__main__':
     print(f"Day 04 pt1 answer: {day_04_pt1_answer(lines=input_lines('input/day_04_cards.txt'))}")
+    print(f"Day 04 pt2 answer: {day_04_pt2_answer(lines=input_lines('input/day_04_cards.txt'))}")
--- aoc23/test/test_day_04.py
+++ aoc23/test/test_day_04.py
@@ -1,7 +1,7 @@
 import unittest
 
 from aoc23.day_03_01 import Schematics, day_03_pt1_answer, day_03_pt2_answer, _gear_ratios
-from aoc23.day_04 import Card, day_04_pt1_answer
+from aoc23.day_04 import Card, day_04_pt1_answer, day_04_pt2_answer
 
 TEST_INPUT = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -10,6 +10,12 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
+TEST_INPUT_PT2 = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
 class TestAocDay04Tests(unittest.TestCase):
 
@@ -42,3 +48,7 @@ class TestAocDay04Tests(unittest.TestCase):
     def test_pt1_should_sum_points(self):
         points = day_04_pt1_answer(lines=TEST_INPUT.splitlines())
         assert points == 13
+
+    def test_pt2_should_sum_points(self):
+        points = day_04_pt2_answer(lines=TEST_INPUT_PT2.splitlines())
+        assert points == 30
